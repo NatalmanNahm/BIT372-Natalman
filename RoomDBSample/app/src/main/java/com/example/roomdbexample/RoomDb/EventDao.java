690ba8282a1de00814df71a4ac45d9e5d13ccde1
NatalmanNahm <natalnahm@gmail.com>
2021-01-18 17:00:17 -0600

    add update method to EventDao class.

--- RoomDBSample/app/src/main/java/com/example/roomdbexample/RoomDb/EventDao.java
+++ RoomDBSample/app/src/main/java/com/example/roomdbexample/RoomDb/EventDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -17,4 +18,8 @@ public interface EventDao {
 
     @Delete
     void deleteEvent(Event event);
+
+    @Update
+    void update(Event event);
+
 }
