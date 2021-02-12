aa0c5c11da1feb2a2f3de5c8e007a46aed3a3b5d
NatalmanNahm <natalnahm@gmail.com>
2021-02-11 20:02:00 -0600

    Add a convertion from lbs to kg to the convert class and use it in the main activity to display conversion in the UI.

--- UnitCoverter/app/src/main/java/com/example/unitcoverter/Converter.java
+++ UnitCoverter/app/src/main/java/com/example/unitcoverter/Converter.java
@@ -4,4 +4,7 @@ public class Converter {
     public static float toCelcius(float fah) {
         return (fah - 32) * 5/9;
     }
+    public static float lbsToKg(float lbs){
+        return (float) (lbs * 0.45359237);
+    }
 }
--- UnitCoverter/app/src/main/java/com/example/unitcoverter/MainActivity.java
+++ UnitCoverter/app/src/main/java/com/example/unitcoverter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.unitcoverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,14 +16,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button fbtn = findViewById(R.id.convert_btn);
+        Button kgBtn = findViewById(R.id.convert_to_kg);
 
         EditText finput = findViewById(R.id.fahrenheit_input);
+        EditText lbsEditText = findViewById(R.id.lbs_editText);
+
         TextView ctxt = findViewById(R.id.celcius_text);
+        TextView kgTextView = findViewById(R.id.kg_convert);
+
         fbtn.setOnClickListener(v -> {
             if (finput.getText().length() == 0)
                 return;
             double celcius = Converter.toCelcius(Float.parseFloat(finput.getText().toString()));
             ctxt.setText(String.format("%.2f ºC", celcius));
+        });
+
+        kgBtn.setOnClickListener(v -> {
+            if (lbsEditText.getText().length() == 0)
+                return;
+            double kg = Converter.lbsToKg(Float.parseFloat(lbsEditText.getText().toString()));
+            kgTextView.setText(String.format("%2f Kg", kg));
         });
     }
 }
